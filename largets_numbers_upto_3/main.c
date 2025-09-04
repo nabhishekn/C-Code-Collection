5ed168fe22d1bb2078f22d8e3ea423cda8c67a9e
nabhishekn <abhisheknn1994@gmail.com>
2025-09-04 22:09:34 +0530

    find top three biggest numbers

---
+++ largets_numbers_upto_3/main.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int arry[5] = {3, -5, 6, 78, 4};
+    int first;
+    int i;
+    first = arry[0];
+    for(i=1; i<5; i++)
+    {
+        if(arry[i] > first)
+        {
+            first = arry[i];
+        }
+      
+    }
+    printf("biggest number is %d\n", first);
+    int second = INT_MIN;
+    
+    for(i=0; i<5; i++)
+    {
+        if((first != arry[i]) && (arry[i] > second))
+        {
+            second = arry[i];
+        }
+    }
+    printf("2nd last biggest number is %d\n", second);
+    
+    int third = INT_MIN;
+    
+    for(i=0; i<5; i++)
+    {
+        if((first != arry[i]) && (second != arry[i]) && (arry[i] > third))
+        {
+            third = arry[i];
+        }
+    }
+    printf("3rd last biggest number is %d", third);
+    
+    return 0;
+}
